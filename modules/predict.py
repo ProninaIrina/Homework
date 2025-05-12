457d7fcf7fc3fdb77a41b3d97ef89707830daa73
Irina <irixa90@gmail.com>
2025-05-12 21:16:22 +0300

    commit message

--- modules/predict.py
+++ modules/predict.py
@@ -1,21 +1,23 @@
 import glob
 import json
 import os
-import dill
+
 import pandas as pd
 
 from datetime import datetime
 
 path = os.environ.get('PROJECT_PATH', '/opt/airflow/plugins')
 
-models_file = glob.glob(f'{path}/data/models/*')
-model_file_path = max(models_file, key=os.path.getctime)
-model_name = model_file_path.split('\\')[-1]
-
-with open(f'{model_name}', 'rb') as file:
-    model = dill.load(file)
 
 def predict():
+    import dill
+
+    models_file = glob.glob(f'{path}/data/models/*')
+    model_file_path = max(models_file, key=os.path.getctime)
+
+    with open(f'{model_file_path}', 'rb') as file:
+        model = dill.load(file)
+
     test_file_name = os.listdir(f'{path}/data/test')
     df = pd.DataFrame()
 
